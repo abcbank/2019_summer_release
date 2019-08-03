db3a129b7d918ad1042020eaba4ede9bfa6f32fe
JD Yoo <woejr0123@gmail.com>
2019-08-03 16:52:32 +0900

    delete code

--- 실험/실험/code/1주차/ex_03.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main() {
-	printf("Data types and sizes\n\n");
-	printf("char = %d bytes\nint = %d bytes\n", sizeof(char), sizeof(int));
-	printf("short = %d bytes\nlong = %d bytes\n", sizeof(short), sizeof(long));
-	printf("float = %d bytes\ndouble = %d bytes\n", sizeof(float), sizeof(double));
-}
